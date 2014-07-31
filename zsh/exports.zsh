f393dea47a5205dc6f5dacfe5f973748db5c01bd
Corey Richardson <corey@octayn.net>
2014-07-30 17:13:00 -0700

    zsh: add new prompt, some misc changes, remove _git

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,9 +1,8 @@
 fpath=(~/.zsh/user-functions $fpath)
 
-export PATH=~/.local/bin:~/.luarocks/bin:$PATH:/opt/android-sdk/tools
+export PATH=~/.local/bin:/opt/android-sdk/tools:$PATH
 
 # Setup terminal, and turn on colors
-export TERM=rxvt-256color
 export CLICOLOR=1
 
 # Enable color in grep
@@ -12,14 +11,20 @@ export GREP_OPTIONS='--color=auto'
 export PAGER='less'
 export EDITOR='vim'
 
-# CTAGS Sorting in VIM/Emacs is better behaved with this in place
-export LC_COLLATE=C 
+# ctags sorting in vim/emacs is better behaved with this in place
+export LANG=en_GB.UTF-8
+export LC_COLLATE=C
 
 export MINICOM="-m -c on"
 
 export CPATH=/usr/local/include
-export GISTY_DIR=~/entropy/gists
 
 export _JAVA_AWT_WM_NONREPARENTING=1
 
-eval $(luarocks path)
+export RUST_PATH=~/.rust/lib
+
+export k=/steam/SteamApps/common/Kerbal\ Space\ Program
+
+export CLASSPATH=".:/usr/share/java/antlr-complete.jar:$CLASSPATH"
+
+export WINEARCH=win64
