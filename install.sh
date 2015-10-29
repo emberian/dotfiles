749f9badfcbc238c163cfecf0b0d9037c5c8f51d
Corey Richardson <corey@octayn.net>
2015-10-29 07:39:55 -0400

    Replace install.sh loops with find
    This script has served well since @xales wrote it, but it's
    not whitespace-in-paths safe and the manual loops can be
    replaced with extant unix tools.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
+set -e
 old_dir=$HOME/.dotfiles_old
 
 if [ ! -f $PWD/README ]; then
@@ -8,84 +9,47 @@ if [ ! -f $PWD/README ]; then
 	exit 1
 fi
 
-files_all=$PWD/*
-files_config=$PWD/config/*
-
-for file in $files_all; do
-	case "$file" in
-		$PWD/README) ;;
-		$PWD/install.sh) ;;
-		$PWD/config*) ;;
-		*) files="$files $file" ;;
-	esac
-done
-
-echo "Backing up existing dotfiles to $old_dir"
-
-if [ -d $old_dir ]; then
-	echo "ERROR: $old_dir already exists!"
+if [ -d "$old_dir" ]; then
+	echo "ERROR: $old_dir already exists! I refuse to clobber the old backups."
 	exit 1
 fi
 
-mkdir $old_dir
-mkdir $old_dir/.config
+function trans_name() {
+if [ $(dirname $1) = "" ] || [ $(dirname $1) = "." ]; then
+    echo ./.$(basename $1)
+else
+    echo $1
+fi
+}
 
-for file in $files; do
-	base=`basename $file`
-	base=".$base"
-	if [ -e $HOME/$base ]; then
-		if [ -L $HOME/$base ]; then
-			if [ `readlink $HOME/$base` = $file ]; then
-				echo "Ignoring existing dotfile $base"
-				continue
-			fi
-		fi
-		echo "Backing up $base"
-	    mv $HOME/$base $old_dir/$base
-	fi
-	echo "Installing link to $base"
-	ln -s $file $HOME/$base
+find . -type f ! -path '*.git/*' -print0 | while IFS= read -r -d $'\0' path; do
+if [ -f "$HOME/$(trans_name $path)" ]; then
+    mkdir -p "$old_dir/$(dirname $path)"
+    cp "$HOME/$(trans_name $path)" "$old_dir/$path"
+fi
 done
 
-if [ ! -d $HOME/.config ]; then
-	mkdir $HOME/.config
-fi
+find . -type d ! -path '*.git/*' -exec mkdir -p "$HOME/{}" \;
 
-for file in $files_config; do
-	base=`basename $file`
-	base=".config/$base"
-	if [ -e $HOME/$base ]; then
-		if [ -L $HOME/$base ]; then
-			if [ `readlink $HOME/$base` = $file ]; then
-				echo "Ignoring existing dotfile $base"
-				continue
-			fi
-		fi
-		echo "Backing up $base"
-		mv $HOME/$base $old_dir/$base
-	fi
-	echo "Installing link to $base"
-	ln -s $file $HOME/$base
+find . -type f ! -path '*.git/*' -print0 | while IFS= read -r -d $'\0' path; do
+ln -sf "$PWD/$path" "$HOME/$(trans_name $path)"
 done
 
-if [ -d $HOME/.vim/bundle/vundle ]; then
-	exit 0
+if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+    # already bootstrapped, don't touch it.
+    true
+else
+    wgit=`which git`
+    wvim=`which vim`
+
+    if [ -z $wvim ]; then
+        echo "Vim not found in PATH; not boostrapping vundle"
+    fi
+
+    if [ -z $wgit ]; then
+        echo "Git not found in PATH; not bootstrapping vundle"
+    fi
+
+    mkdir -p $HOME/.vim/bundle/Vundle.vim
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-
-wgit=`which git`
-wvim=`which vim`
-
-if [ -z $wvim ]; then
-	echo "Vim not found in PATH; not boostrapping vundle"
-	exit 0
-fi
-
-if [ -z $wgit ]; then
-	echo "Git not found in PATH; not bootstrapping vundle"
-	exit 0
-fi
-
-echo "Bootstrapping vundle to $HOME/.vim"
-
-mkdir -p $HOME/.vim/bundle/Vundle.vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
